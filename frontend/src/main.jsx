128cae18dbeeb69b504d8426b45e0d2a495a0918
Shazmina <shxminaoud@gmail.com>
2026-03-02 10:07:21 +0530

    created homepage, setup tailwindcss to fit theme colour and setup dark and light mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import TestBackend from "./TestBackend";
+import App from "./App";
+import { ThemeProvider } from './context/ThemeContext';
+import HomePage from "./Pages/Home/homepage";  // If in pages folder
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TestBackend />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
