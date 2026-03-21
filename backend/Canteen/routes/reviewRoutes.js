1df7fd07f4c40d569311d85b829f14a9ce966993
Lakindu Nawanearachchi <182813570+Dhana-11-creator@users.noreply.github.com>
2026-03-21 16:01:37 +0530

    Revert "Merge branch 'main' into User-Management-&-Authorization"
    This reverts commit 87ce2c25ae368c70d6f3ce4a24700051c03e77b4, reversing
    changes made to 5c8796e9c5dc687013a0b64312c8d568e2b56bde.

--- backend/Canteen/routes/reviewRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
- const { getReviews, replyToReview } = require('../controllers/reviewController');
- const router = express.Router();
-
- router.get('/',               getReviews);
- router.patch('/:id/reply',    replyToReview);
- module.exports = router;
