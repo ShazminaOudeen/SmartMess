ff7be70c1a5e6564b0ca8f31fdd17ae173ee2d27
Lakindu Nawanearachchi <182813570+Dhana-11-creator@users.noreply.github.com>
2026-03-21 16:01:37 +0530

    Revert "Merge branch 'main' into User-Management-&-Authorization"
    This reverts commit 87ce2c25ae368c70d6f3ce4a24700051c03e77b4, reversing
    changes made to 5c8796e9c5dc687013a0b64312c8d568e2b56bde.

--- backend/Admin/routes/analyticsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getCanteenAnalytics, getMonthlyTrend } = require('../controllers/analyticsController');
-const router = express.Router();
-
-router.get('/analytics/canteens',      getCanteenAnalytics);
-router.get('/analytics/monthly-trend', getMonthlyTrend);
-
-module.exports = router;
