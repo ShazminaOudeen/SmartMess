9f64f2800408fcacd6d0c90c4f7b5d8fb855682a
Shazmina <shxminaoud@gmail.com>
2026-04-18 21:21:04 +0530

    blocking user bug fixed

--- backend/Auth/middleware/authMiddleware.js
+++ backend/Auth/middleware/authMiddleware.js
@@ -39,6 +39,14 @@ const protect = async (req, res, next) => {
             });
         }
 
+         if (req.user.isBlocked) {
+            return res.status(403).json({
+                success: false,
+                message: 'Your account has been blocked. Please contact support.',
+            });
+        }
+
+
         next();
     } catch (error) {
         return res.status(401).json({
